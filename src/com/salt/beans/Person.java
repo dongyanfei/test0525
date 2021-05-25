3080d0574d2a1a84c9da1151c7c46ea58e0920ad
dongyanfei <dongyanfei@longshine.com>
2021-05-25 16:37:35 +0800

    第一次提交

---
+++ src/com/salt/beans/Person.java
@@ -0,0 +1,15 @@
+package com.salt.beans;
+
+/**
+ * @ClassName: Person
+ * @Package: com.salt.beans
+ * @Description:
+ * @Author: dyf
+ * @Create:2021/5/25 16:13
+ */
+
+
+public class Person {
+
+    private String name;
+}
